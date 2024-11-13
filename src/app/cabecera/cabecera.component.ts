c201341d83d56af29969b5917f82987841d44e77
angel452 <angelloayzah2016@gmail.com>
2024-11-13 10:56:33 -0500

    Add CabeceraComponent for budget overview; display income and expenses

---
+++ src/app/cabecera/cabecera.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecera',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cabecera.component.html',
+  styleUrl: './cabecera.component.css'
+})
+export class CabeceraComponent {
+  @Input() presupuestoTotal! : number;
+  @Input() porcentajeTotal! : number;
+  @Input() egresoTotal! : number;
+  @Input() ingresoTotal! : number;
+}
